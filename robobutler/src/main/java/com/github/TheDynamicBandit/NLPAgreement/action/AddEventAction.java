1cb8aab866b959ad5fa8eb0304535266699b4e20
zrbonds <zrbonds@ncsu.edu>
2020-07-30 22:08:14 -0400

    Continued implementation of NLP event planner
    Problems Identified: NLP date and time model doesn't recognize numerical
    dates or times. We may need to train our own model.

--- robobutler/src/main/java/com/github/TheDynamicBandit/NLPAgreement/action/AddEventAction.java
+++ robobutler/src/main/java/com/github/TheDynamicBandit/NLPAgreement/action/AddEventAction.java
@@ -4,14 +4,18 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.LocalTime;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.javacord.api.event.message.MessageCreateEvent;
 
+import com.github.TheDynamicBandit.event.Event;
 import com.github.TheDynamicBandit.manager.EventManager;
+import com.github.TheDynamicBandit.reminder.Reminder;
 
 import opennlp.tools.namefind.NameFinderME;
 import opennlp.tools.namefind.TokenNameFinderModel;
@@ -55,16 +59,56 @@ public class AddEventAction extends Action {
 		getKeywords().add(new Keyword(reminderWeight, "reminder"));
 		getKeywords().add(new Keyword(setWeight, "set"));
 	}
+	
+	/**
+	 * A private method for handling issues in the code
+	 */
+	private void errorHandler(MessageCreateEvent event, String errorDescription) {
+		event.getChannel().sendMessage("I'm sorry Master " + event.getMessageAuthor().getName()
+				+ ", something has gone horribly, horribly wrong.");
+		System.out.println(errorDescription);
+	}
 
 	@Override
 	public void execute(MessageCreateEvent event, EventManager manager) {
+		// Try to find a date
 		String[] dates = dateExtractor(event.getMessageContent());
 		if(dates == null) {
-			event.getChannel().sendMessage("I'm sorry Master " + event.getMessageAuthor().getName()
-					+ ", something has gone horribly, horribly wrong.");
+			errorHandler(event, "OpenNLP critical failure");
 			return;
 		}
-
+		// Resolve any date conflicts or lack of dates
+		if(dates.length == 0) {
+			errorHandler(event, "No dates returned by dateExtractor");
+			return;
+		}
+		// Parse the date into a local date time
+		LocalDateTime reminderDate = null;
+		try {
+			reminderDate = LocalDateTime.parse(dates[0]);
+		}
+		catch(DateTimeParseException e) {
+			errorHandler(event, "Date returned by dateExtractor not successfully parsed");
+			return;
+		}
+		if(reminderDate == null) {
+			errorHandler(event, "Date returned by dateExtractor not successfully parsed");
+			return;
+		}
+		// Try to get a time 
+		String[] times = timeExtractor(event.getMessageContent());
+		if(times != null && times.length != 0) {
+			LocalTime time = LocalTime.parse(times[0]);
+			reminderDate = LocalDateTime.of(reminderDate.toLocalDate(), time);
+		}
+		
+		// Now we're done parsing the date
+		// Create a new Event
+		Event reminderEvent = new Event(reminderDate, event.getMessageContent());
+		// Add it to the manager
+		manager.addEventToManager(reminderEvent);
+		// Create a reminder
+		new Reminder(reminderDate, event.getMessage().getUserAuthor().get(), event.getChannel(), event.getMessageContent());
 	}
 
 	/**
@@ -118,5 +162,57 @@ public class AddEventAction extends Action {
 		// And convert those spans to strings
 		String[] dates = Span.spansToStrings(nameSpans, tokens);
 		return dates;
+	}
+	
+	/**
+	 * A helper method to extract the time from the Message String. Made temporarily public for testing purposes.
+	 * 
+	 * @param message The message from which to extract a time
+	 * @return The String[] of possible times
+	 */
+	public String[] timeExtractor(String message) {
+		// Try to build the models, let us know if something goes fudgy
+		TokenizerModel tokenModel = null;
+		TokenNameFinderModel nameModel = null;
+		InputStream modelIn1 = null;
+		InputStream modelIn2 = null;
+		try {
+			modelIn1 = new FileInputStream("src/en-ner-time.bin");
+			modelIn2 = new FileInputStream("src/en-token.bin");
+			tokenModel = new TokenizerModel(modelIn2);
+			nameModel = new TokenNameFinderModel(modelIn1);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		} finally {
+			if (modelIn1 != null) {
+				try {
+					modelIn1.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			if (modelIn2 != null) {
+				try {
+					modelIn2.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// If nothing goes horribly, horribly wrong, set up our tokenizer and nameFinder
+		Tokenizer tokenizer = new TokenizerME(tokenModel);
+		NameFinderME nameFinder = new NameFinderME(nameModel);
+
+		// Get our tokens
+		String tokens[] = tokenizer.tokenize(message);
+
+		// Send it to an array of Span objects
+		Span nameSpans[] = nameFinder.find(tokens);
+
+		// And convert those spans to strings
+		String[] times = Span.spansToStrings(nameSpans, tokens);
+		return times;
 	}
 }
--- robobutler/src/test/java/com/github/TheDynamicBandit/NLPAgreement/action/AddEventActionTest.java
+++ robobutler/src/test/java/com/github/TheDynamicBandit/NLPAgreement/action/AddEventActionTest.java
@@ -9,8 +9,15 @@ public class AddEventActionTest {
 	@Test
 	public void testDateExtractor() {
 		AddEventAction e = new AddEventAction();
-		String[] dates = e.dateExtractor("January 21st, 2001 is my birthday");
+		String[] dates = e.dateExtractor("Create a reminder 7/31/2020");
 		assertTrue(dates != null);
+	}
+	
+	@Test
+	public void testTimeExtractor() {
+		AddEventAction e = new AddEventAction();
+		String[] times = e.timeExtractor("My birthday is 10 minutes from now. The time is midnight. Set a reminder for January 1st, at 11:30am");
+		assertTrue(times != null);
 	}
 
 }
